4a6957b9692d98fe0767b3b35b18989120e79b58
Victoria Slocum <victorialslocum@gmail.com>
2023-01-05 11:44:54 -0500

    refactor

--- scripts/data.py
+++ scripts/data.py
@@ -1,26 +1,21 @@
 import arxiv
 import srsly
 
-# search 1: key words in title, key phrase in abstract
-search_1 = arxiv.Search(
-    query='ti:dataset OR ti:corpus OR ti:database OR abs:"a new dataset"',
-    max_results=400,
-    sort_by=arxiv.SortCriterion.SubmittedDate,
-)
+queries = {
+    'ti:dataset OR ti:corpus OR ti:database OR abs:"a new dataset"': 400,
+    "ti:data": 100
+}
 
-# search 2: more broad search to get more outliers
-search_2 = arxiv.Search(
-    query="ti:data",
-    max_results=100,
-    sort_by=arxiv.SortCriterion.SubmittedDate,
-)
-
-# initialize dataset
 dataset = []
-
-# iterate through results and append title, summary and link to the dataset
-for search in [search_1, search_2]:
-    for result in search.results():
+for query, n in queries.items():
+    # Start the query
+    items = arxiv.Search(
+        query=query,
+        max_results=n,
+        sort_by=arxiv.SortCriterion.SubmittedDate,
+    )
+    # Add items to dataset
+    for item in items.results():
         summary = str(result.summary).replace("\n", " ")
         dataset.append(
             {
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -3,19 +3,14 @@ import srsly
 # import raw arxiv data
 raw_dataset = srsly.read_jsonl("assets/raw/raw_data.jsonl")
 
-# initialize dataset
-process_data = []
-
-# iterate through papers and refactor data into correct form
-for paper in raw_dataset:
-    process_data.append(
-        {
-            "text": f"{paper['title']}\n{paper['summary']}",
-            "title": paper["title"],
-            "summary": paper["summary"],
-            "meta": {"link": paper["link"]},
-        }
-    )
+def process(paper):
+    return {
+        "text": f"{paper['title']}\n{paper['summary']}",
+        "title": paper["title"],
+        "summary": paper["summary"],
+        "meta": {"link": paper["link"]},
+    }
 
 # write data
-srsly.write_jsonl("assets/data.jsonl", process_data)
+processed_data = [process(p) for p in process_data]
+srsly.write_jsonl("assets/data.jsonl", processed_data)
