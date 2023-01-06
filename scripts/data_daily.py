fb4b713e6da94362bcde0b329b7fabceff72267d
Victoria Slocum <victorialslocum@gmail.com>
2023-01-06 13:42:49 -0500

    add daily data collection script

---
+++ scripts/data_daily.py
@@ -0,0 +1,35 @@
+import arxiv
+import srsly
+from datetime import date
+from datetime import timedelta
+
+today = date.today()
+yesterday = today - timedelta(days = 1)
+
+queries = {
+    'ti:dataset OR ti:corpus OR ti:database OR abs:"a new dataset"': 30,
+    "ti:data": 20
+}
+
+dataset = []
+for query, n in queries.items():
+    # Start the query
+    items = arxiv.Search(
+        query=query,
+        max_results=n,
+        sort_by=arxiv.SortCriterion.SubmittedDate,
+    )
+    # Add items to dataset
+    for result in items.results():
+        if str(result.updated)[0:10] == str(yesterday):
+            summary = str(result.summary).replace("\n", " ")
+            dataset.append(
+                {
+                    "title": result.title,
+                    "summary": summary,
+                    "link": result.entry_id,
+                }
+            )
+
+# write file
+srsly.write_jsonl(f"./assets/raw/raw_data_{str(yesterday)}.jsonl", dataset)
