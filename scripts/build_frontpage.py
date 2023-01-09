5aedfd58534e348d9928e1115d8dd823ac27698f
Vincent <vincentwarmerdam@gmail.com>
2023-01-09 17:36:01 +0100

    better css

--- scripts/build_frontpage.py
+++ scripts/build_frontpage.py
@@ -13,8 +13,8 @@ template = env.from_string("""
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div class="relative mx-auto h-full max-w-prose text-lg">
-    <h1 class="text-6xl pt-4 font-bold">Your FrontPage</h1>
+    <div class="relative mx-auto h-full max-w-prose text-md">
+    <h1 class="text-6xl pt-4 font-bold"><span class="underline">Your</span> FrontPage</h1>
     <br>
     {{links}}
     </div>
@@ -26,7 +26,7 @@ def main(content):
     content_stream = srsly.read_jsonl(content)
 
     def make_elem(item):
-        result = f"""<a href='{item['link']}'>{item['title']}"""
+        result = f"""<a class="hover:underline decoration-2 decoration-green-600" href='{item['link']}'>{item['title']}"""
         for tag in item['tags']:
             result += f"<span class='px-2 mx-2 bg-gray-200'>{tag}</span>"
         return f"{result}</a>" 
