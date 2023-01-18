9b574088e837f7038f943915e75e69e77d180fe6
Victoria Slocum <victorialslocum@gmail.com>
2023-01-18 17:35:47 -0500

    patterns to jsonl, textcat for mult tags, clean

--- recipes/textcat_topic.py
+++ recipes/textcat_topic.py
@@ -3,7 +3,6 @@ import spacy
 import srsly
 from prodigy import set_hashes
 from prodigy.components.loaders import JSONL
-from spacy.matcher import Matcher
 
 
 @prodigy.recipe(
@@ -12,33 +11,38 @@ from spacy.matcher import Matcher
     examples=("Examples to load from disk", "positional", None, str),
     model=("spaCy model to load", "positional", None, str),
     patterns=("Patterns to match from json file", "positional", None, str),
-    tag=("Tag to filter", "positional", None, str)
+    tags=("Tags to filter items from the dataset", "positional", None, str),
+    label=("Label for annotated data", "positional", None, str),
 )
-def textcat_topic(dataset, examples, model, patterns, tag):
+def textcat_topic(dataset, examples, model, patterns, tags, label):
     # import spaCy and initialize matcher
     nlp = spacy.load(model)
-    matcher = Matcher(nlp.vocab)
+
+    # create list of tags
+    tags = [tag for tag in tags.split(",")]
 
     # set up stream and set hashes just on text key
-    stream = (item for item in JSONL(examples) if tag in item["tags"])
+    stream = (
+        item for item in JSONL(examples) if any(tag in tags for tag in item["tags"])
+    )
     stream = (set_hashes(ex, input_keys=("title")) for ex in stream)
 
     # add matcher pattern to underline
-    patterns = srsly.read_json(patterns)
-    matcher.add(tag, patterns)
+    patterns = srsly.read_jsonl(patterns)
+    ruler = nlp.add_pipe("span_ruler")
+    ruler.add_patterns(patterns)
 
     # Render title and description in HTML format for Prodigy as a generator object
     def add_html(examples):
         for ex in examples:
             doc = nlp(ex["description"])
-            matches = matcher(doc)
 
             summary_highlight = ex["description"]
-            for match_id, start, end in matches:
-                span = doc[start:end]  # The matched span
+            for span in doc.spans["ruler"]:
                 summary_highlight = summary_highlight.replace(
                     span.text, f"<b style='background-color: yellow;'>{span.text}</b>"
                 )
+
             ex[
                 "html"
             ] = f"<h3>{ex['title']}</h3><p><font size='3'>{summary_highlight}</font></p><a href='{ex['link']}'>LINK</a>"
@@ -47,7 +51,7 @@ def textcat_topic(dataset, examples, model, patterns, tag):
     # delete html key in output data and add label key
     def before_db(examples):
         for ex in examples:
-            ex["label"] = tag
+            ex["label"] = label
             ex["text"] = ex["title"] + "\n" + ex["description"]
             del ex["html"]
             del ex["title"]
--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,40 +1,43 @@
 from collections import Counter
-import typer
+from pathlib import Path
+
 import spacy
 import srsly
+import typer
 from prodigy.components.db import connect
-from spacy.matcher import Matcher
-from pathlib import Path
 from rich.console import Console
 from rich.table import Table
+
 
 def main(
     dataset: str = typer.Argument(..., help="Name of the Prodigy dataset"),
     patterns: Path = typer.Argument(..., help="Path to patterns file"),
-    model: Path = typer.Argument(..., help="Path to spaCy model")
+    model: Path = typer.Argument(..., help="Path to spaCy model"),
 ):
     """Evaluate matcher and spaCy model."""
-    
+
     nlp = spacy.load("en_core_web_sm")
     db = connect()
 
     dataset = db.get_dataset_examples(dataset)
 
-    label = dataset[0]['label']
+    label = dataset[0]["label"]
     results = {}
 
-    matcher = Matcher(nlp.vocab)
-    patterns = srsly.read_json(patterns)
-    matcher.add(label, patterns)
+    patterns = srsly.read_jsonl(patterns)
+    ruler = nlp.add_pipe("span_ruler")
+    ruler.add_patterns(patterns)
 
     def matcher_evaluate(text):
         doc = nlp(text)
-        matches = matcher(doc)
+        matches = doc.spans["ruler"]
         if len(matches) > 0:
             return True
         return False
 
-    matcher_results = Counter([(matcher_evaluate(d["text"]), d["answer"]) for d in dataset]).items()
+    matcher_results = Counter(
+        [(matcher_evaluate(d["text"]), d["answer"]) for d in dataset]
+    ).items()
     results["matcher"] = matcher_results
 
     model_path = Path(model) / "model-best"
@@ -47,14 +50,29 @@ def main(
             return True
         return False
 
-    model_results = Counter([(model_evaluate(d["text"]), d["answer"]) for d in dataset]).items()
+    model_results = Counter(
+        [(model_evaluate(d["text"]), d["answer"]) for d in dataset]
+    ).items()
     results["model"] = model_results
 
-    table = Table("Name", "False, negative","True, positive", "False, positive", "True, negative", "Accuracy (%)", title="Values")
+    table = Table(
+        "Name",
+        "False, negative",
+        "True, positive",
+        "False, positive",
+        "True, negative",
+        "Accuracy (%)",
+        title="Values",
+    )
 
     def get_values(counter):
-        columns = [(False, 'reject'), (True, 'accept'), (False, 'accept'), (True, 'reject')]
-        
+        columns = [
+            (False, "reject"),
+            (True, "accept"),
+            (False, "accept"),
+            (True, "reject"),
+        ]
+
         rows = [value for key, value in sorted(counter) if key in columns]
 
         acc = round(((rows[1] + rows[2]) / sum(rows)) * 100, 2)
@@ -62,12 +80,13 @@ def main(
 
     for key in results:
         rows, acc = get_values(results[key])
-        table.add_row(key, str(rows[1]), str(rows[2]), str(rows[0]), str(rows[3]), str(acc))
+        table.add_row(
+            key, str(rows[1]), str(rows[2]), str(rows[0]), str(rows[3]), str(acc)
+        )
 
     console = Console()
     console.print(table)
 
+
 if __name__ == "__main__":
     typer.run(main)
-
-
