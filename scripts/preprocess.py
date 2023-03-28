a8e57e6a5698cc29e4f3f5a2460b40678f466ffa
Vincent <vincentwarmerdam@gmail.com>
2023-03-28 15:54:21 +0200

    improved pattern

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -7,13 +7,17 @@ from prodigy import set_hashes
 from prodigy.components.filters import filter_duplicates
 from schemas import Content
 
+def remove_meta(d):
+    if 'meta' in d:
+        del d['meta']
+    return d
 
 def main(folder: Path, out: Path):
     """Concat all files and double-check the schema."""
     glob = folder.glob("**/*.jsonl")
     full_data = it.chain(*list(srsly.read_jsonl(file) for file in glob))
 
-    stream = (set_hashes(eg) for eg in full_data)
+    stream = (set_hashes(remove_meta(eg)) for eg in full_data)
     stream = filter_duplicates(stream, by_input=True, by_task=True)
     stream = (dict(Content(**item)) for item in stream)
     srsly.write_jsonl(out, full_data)
